fbb5a6e36523272f2bb4c4ef25fbc9cfc9da834c
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-07-30 11:57:55 +0200

    Update main.tf

--- VW/Terraform/main.tf
+++ VW/Terraform/main.tf
@@ -354,18 +354,16 @@ resource "aws_eks_cluster" "eks" {
   
   
   resource "aws_eks_node_group" "eks-node-group" {
-  cluster_name    = var.cluster-name
-  node_group_name = "${var.cluster-name}-default-node-group"
+  cluster_name    = "eks-cluster"
+  node_group_name = "eks-cluster-default-node-group"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = data.aws_subnet_ids.private.ids
   scaling_config {
-    desired_size = var.desired-capacity
-    max_size     = var.max-size
-    min_size     = var.min-size
+    desired_size = 2
+    max_size     = 5
+    min_size     = 1
   }
-  instance_types = [
-    var.node-instance-type
-  ]
+  instance_types = t3.micro
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
@@ -374,6 +372,6 @@ resource "aws_eks_cluster" "eks" {
     aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy
   ]
   tags = {
-    Name = "${var.cluster-name}-default-node-group"
+    Name = "eks-cluster-default-node-group"
   }
 }
