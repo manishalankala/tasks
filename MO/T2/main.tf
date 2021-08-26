c29c8a730b2406a545391278643fae59cc9d9e39
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-08-26 16:12:16 +0200

    Update main.tf

--- MO/T2/main.tf
+++ MO/T2/main.tf
@@ -18,3 +18,46 @@ provider "azurerm" {
   tenant_id       = var.azure-tenant-id
 }
 
+
+
+###########################
+# Create a Resource Group
+###########################
+
+resource "azurerm_resource_group" "rg" {
+  name     = "dev-rg"
+  location = var.location
+}
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "dev-vnet"
+  address_space       = [var.hub-vnet]
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+}
+
+
+###########################
+# Create a Gateway Subnet
+###########################
+
+resource "azurerm_subnet" "gateway-subnet" {
+  name                 = "GatewaySubnet" # do not rename
+  address_prefixes     = [var.gateway-subnet]
+  virtual_network_name = azurerm_virtual_network.hub-vnet.name
+  resource_group_name  = azurerm_resource_group.hub-rg.name
+}
+
+
+###########################
+# Create a Hub Subnet
+###########################
+
+resource "azurerm_subnet" "psubnet" {
+  name                 = "private-subnet" # do not rename
+  address_prefixes     = [var.hub-resources-subnet]
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+}
+
