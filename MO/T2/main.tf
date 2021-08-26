288b48092a65c41f006dd6a89543824fc34887c8
manish kumar alankala <33985509+manishalankala@users.noreply.github.com>
2021-08-26 17:03:10 +0200

    Update main.tf

--- MO/T2/main.tf
+++ MO/T2/main.tf
@@ -67,3 +67,33 @@ resource "azurerm_network_security_group" "nsg" {
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 }
+
+
+resource "azurerm_network_security_rule" "rulessh" {
+  name                        = "dev-rulessh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${azurerm_resource_group.rg.name}"
+  network_security_group_name = "${azurerm_network_security_group.nsg.name}"
+}
+
+
+resource "azurerm_network_security_rule" "rulehttps" {
+  name                        = "dev-rulehttps"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${azurerm_resource_group.rg.name}"
+  network_security_group_name = "${azurerm_network_security_group.nsg.name}"
+}
