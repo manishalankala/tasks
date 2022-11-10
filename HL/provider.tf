f97a2201ca2ffd029c9d660aa130fb0e501f97fb
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-10 21:41:38 +0100

    Update provider.tf

--- HL/provider.tf
+++ HL/provider.tf
@@ -7,3 +7,10 @@ terraform {
     }
   }
 }
+
+#AWS Provider 
+provider "aws" {
+  access_key = "${var.aws_access_key_id}"
+  secret_key = "${var.aws_secret_access_key}"
+  region     = "${var.vpc_region}"
+}
