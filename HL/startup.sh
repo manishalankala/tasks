16b6ace319dfcd3d446be4acf8fc1eb92d142755
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-10 20:55:11 +0100

    Update startup.sh

--- HL/startup.sh
+++ HL/startup.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-
-
 ##### Install Dependencies ######
 
 apt-get update -y
 apt-get install git vim -y
 
 
-##### if need to  clone repository  ######
-### git clone 
-
 ##### RUN ansible playbook #####
-
-ansible-playbook -i hosts ./ansible/nginx/
+## ansible-playbook -i hosts ./ansible/nginx/
