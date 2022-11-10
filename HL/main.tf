7c4aaf5c7a35a1d54dc13c308135835d6dd51145
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-10 21:48:37 +0100

    Update main.tf

--- HL/main.tf
+++ HL/main.tf
@@ -1,9 +1,4 @@
 
 
-#AWS Provider 
-provider "aws" {
-  access_key = "${var.aws_access_key_id}"
-  secret_key = "${var.aws_secret_access_key}"
-  region     = "${var.vpc_region}"
-}
 
+resource "aws_instance" "vpn_server" {
