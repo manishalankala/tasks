18e6ab003e4eae0f2a290ada7269e4c09f4b0143
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-10 23:17:42 +0100

    Update networking.tf

--- HL/networking.tf
+++ HL/networking.tf
@@ -165,6 +165,22 @@ resource "aws_launch_configuration" "nginx" {
   instance_type   = "t2.medium"
   user_data       = file("startup.sh")
   security_groups = [aws_security_group.nginx_sg.id]
+  
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = var.ssh_user
+      private_key = file(var.private_key_path)
+      host        = aws_instance.nginx.public_ip
+    }
+  }
+  
+  
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key ${var.private_key_path} nginx.yaml"
+  }
 
   lifecycle {
     create_before_destroy = true
