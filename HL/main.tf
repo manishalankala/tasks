e2e35a7edb93779785539fd9fd07c61bbaec7847
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-10 22:02:41 +0100

    Update main.tf

--- HL/main.tf
+++ HL/main.tf
@@ -2,3 +2,14 @@
 
 
 resource "aws_instance" "vpn_server" {
+  ami     = "ami-0764964fdfe99bc31"
+  instance_type          = "t2.medium" 
+  user_data = <<-EOF
+              admin_user=${var.server_username}
+              admin_pw=${var.server_password}
+              EOF
+  
+  tags = {
+    Name = "vpn"
+  }
+}
