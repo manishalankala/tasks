13a7252cf8de35bd62f3da2225dd5271750b2ed1
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-10 22:05:02 +0100

    Update variables.tf

--- HL/variables.tf
+++ HL/variables.tf
@@ -2,3 +2,16 @@ variable "aws_region" {
   description = "The AWS region to use"
   default     = "eu-central-1"
 }
+
+
+variable "vpn_username" {
+  description = "Admin Username to access server"
+  type        = string
+  default     = "openvpn"
+}
+
+variable "vpn_password" {
+  description = "Admin Password to access server"
+  type        = string
+  default     = "password"
+}
