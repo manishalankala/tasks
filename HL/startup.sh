ef8ecf17cfa7cbd9622d40b0566c98cf269a097e
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-11 22:28:23 +0100

    Update startup.sh

--- HL/startup.sh
+++ HL/startup.sh
@@ -3,7 +3,7 @@
 ##### Install Dependencies ######
 
 apt-get update -y
-apt-get install git vim curl wget software-properties-common -y
+apt-get install git vim curl wget software-properties-common python3.6 -y
 
 
 ##### RUN ansible playbook #####
