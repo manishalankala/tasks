7b16fa97747ff9f9be1eafb86ed3cf61767b8e82
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-11 22:52:53 +0100

    Update startup.sh

--- HL/startup.sh
+++ HL/startup.sh
@@ -2,8 +2,17 @@
 
 ##### Install Dependencies ######
 
-apt-get update -y
-apt-get install git vim curl wget software-properties-common python3.6 -y
+
+sudo amazon-linux-extras install -y epel
+sudo apt-get update -y
+sudo apt-get install git vim curl wget -y
+sudo apt-get install software-properties-common python3.6 -y
+sudo useradd ubuntu
+echo -e 'ubuntu\nubuntu' | sudo passwd ubuntu
+echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' | sudo tee /etc/sudoers.d/ubuntu
+sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+sudo systemctl restart sshd.service
+
 
 
 ##### RUN ansible playbook #####
