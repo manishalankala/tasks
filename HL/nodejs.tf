59bae86b177db74ab515b7556bacae2e506ba249
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-11 22:46:07 +0100

    Update nodejs.tf

--- HL/nodejs.tf
+++ HL/nodejs.tf
@@ -6,11 +6,10 @@ resource "aws_launch_configuration" "nodejs" {
   name_prefix     = "nodejs-"
   image_id        = "xxxxxxxxx "
   instance_type   = "t2.medium"
-  user_data       = file("startup.sh")
   security_groups = [aws_security_group.nginx_sg.id]
   
  provisioner "file" {
-    source      = "/start.sh"
+    source      = "HL/start.sh"
     destination = "/tmp/start.sh"
   connection {
     type        = "ssh"
@@ -19,13 +18,35 @@ resource "aws_launch_configuration" "nodejs" {
     host        = aws_instance.nginx.public_ip
     }
   }
+
   
+ provisioner "remote-exec" {
+    inline = [
+      "sudo ln -s /usr/bin/python3 /usr/bin/python",
+      "chmod +x /tmp/start.sh",
+      "./tmp/wait-for-cloud-init.sh",
+    ]
+  }
   
-  
-  provisioner "local-exec" {
-    command = "ansible-playbook --private-key ${var.private_key_path} /path/nodejs.yaml"
+ provisioner "local-exec" {
+    command = "ansible-playbook --private-key ${var.private_key_path} /HL/nodejs.yaml"
   }
 
+#    # Execute Ansible Playbook
+# provisioner "remote-exec" {
+#    inline = [
+#      "sleep 120; ansible-playbook nginx.yaml"
+#    ]
+#    connection {
+#      type        = "ssh"
+#      user        = "ec2-user"
+#      private_key = file(pathexpand(var.ssh_key_pair))
+#      host        = self.public_ip
+#    }
+#  }
+  
+  
+  
   lifecycle {
     create_before_destroy = true
   }
