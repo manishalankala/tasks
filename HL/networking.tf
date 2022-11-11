4bacc8947d51483d678863729cb21a6bc143929a
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-11 22:58:42 +0100

    Update networking.tf

--- HL/networking.tf
+++ HL/networking.tf
@@ -1,3 +1,10 @@
+### Key ####
+
+resource "aws_key_pair" "ec2loginkey" {
+  key_name = "login-key"
+  ## change here if you are using different key pair
+  public_key = file(pathexpand(var.ssh_key_pair_pub))
+}
 
 
 ###### VPC ######
