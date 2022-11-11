75f187505532f3082d8bd669d2ab13ad6fe001b8
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-11 15:42:57 +0100

    Update networking.tf

--- HL/networking.tf
+++ HL/networking.tf
@@ -120,7 +120,7 @@ resource "aws_route_table_association" "vpc_private_sn_rt_assn" {
 
 
 
-####### security group #######
+####### security group Nginx #######
 
 resource "aws_security_group" "nginx_sg" {
 
@@ -152,4 +152,45 @@ resource "aws_security_group" "nginx_sg" {
     tags = {
         Name = "nginx_sg"
     }
+}
+
+
+####### security group Mongodb #######
+
+resource "aws_security_group" "mongo_sg" {
+  name   = "mongo_sg"
+  vpc_id = "${aws_vpc.vpc_name.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.vpc_cidr_block}"]
+  }
+
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["${var.vpc_cidr_block}"]
+  }
+
+  ingress {
+  from_port   = -1
+  to_port     = -1
+  protocol = "icmp"
+  cidr_blocks  = ["${var.vpc_cidr_block}"]
+}
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Mongo_Security_Group"
+  }
 }
