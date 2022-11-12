b2e73073c5285ccfb46519ea42b3cc187092cb2f
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-12 22:47:22 +0100

    Create variables.tf

---
+++ HL/modules/variables.tf
@@ -0,0 +1,15 @@
+variable "dbUser" {
+  default = "xxxxx"
+}
+variable "dbUserPass" {
+  default = "xxxxxxx"
+}
+variable "dbReplicaAdmin" {
+  default = "replicaAdmin"
+}
+variable "dbReplicaAdminPass" {
+  default = "replicaAdminxxxxxx"
+}
+variable "dbReplSetName" {
+  default = "replicaset"
+}
