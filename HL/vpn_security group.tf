4020ea3e41b7e8477b0c906c1237da7e247eec3d
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 00:03:50 +0100

    Create vpn_security group.tf

---
+++ HL/vpn_security group.tf
@@ -0,0 +1,44 @@
+resource "aws_security_group" "vpn_sg" {
+
+    vpc_id = "vpc"
+    name   = "vpn_sg"
+  
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 943
+        to_port = 943
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    ingress {
+        from_port = 1194
+        to_port = 1194
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "vpn_sg"
+    }
+}
