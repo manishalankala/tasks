3f3bd70b464e0735cf40e92daaf6d90cc5d1c1e8
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 00:06:43 +0100

    Create subnets.tf

---
+++ HL/subnets.tf
@@ -0,0 +1,11 @@
+###### Public subnet ######
+
+rresource "aws_subnet" "vpc_public_subnet_1" {
+  vpc_id            = "vpc"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  map_public_ip_on_launch = "true"
+  tags {
+    Name = "${var.vpc_name}_vpc_public_subnet_1"
+  }
+}
