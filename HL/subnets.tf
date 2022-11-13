d35b8964e1fc9e708cec1fd0b6137fa1e6e8d27c
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 01:18:14 +0100

    Update subnets.tf

--- HL/subnets.tf
+++ HL/subnets.tf
@@ -11,7 +11,7 @@ rresource "aws_subnet" "vpc_public_subnet_1" {
 }
 
 
-rresource "aws_subnet" "vpc_public_subnet_2" {
+resource "aws_subnet" "vpc_public_subnet_2" {
   vpc_id            = "vpc"
   cidr_block        = "10.0.2.0/24"
   availability_zone = "${data.aws_availability_zones.available.names[0]}"
@@ -20,3 +20,16 @@ rresource "aws_subnet" "vpc_public_subnet_2" {
     Name = "${var.vpc_name}_vpc_public_subnet_1"
   }
 }
+
+resource "aws_subnet" "vpc_public_subnet_3" {
+  vpc_id            = "vpc"
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  map_public_ip_on_launch = "true"
+  tags {
+    Name = "${var.vpc_name}_vpc_public_subnet_3"
+  }
+}
+
+
+
