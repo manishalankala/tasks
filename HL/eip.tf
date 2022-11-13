dfaf1ebc4b4a0e805f2a7601f82c06892601908f
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 01:24:03 +0100

    Create eip.tf

---
+++ HL/eip.tf
@@ -0,0 +1,22 @@
+#### vpn with public ip association ####
+
+resource "aws_eip" "vpn_eip" {
+  instance = aws_instance.vpn.id
+  vpc = true
+} 
+
+
+#### alb with public ip association (optional) ####
+
+resource "aws_eip" "alb_eip1" {
+  instance = aws_lb.alb.id
+  vpc = true
+} 
+
+
+#### alb with public ip association (optional) ####
+
+resource "aws_eip" "alb_eip2" {
+  instance = aws_lb.alb.id
+  vpc = true
+} 
