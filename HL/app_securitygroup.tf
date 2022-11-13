0f39605762a8fde1345269b1499df275670c47d8
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 02:10:13 +0100

    Update app_securitygroup.tf

--- HL/app_securitygroup.tf
+++ HL/app_securitygroup.tf
@@ -1,20 +1,20 @@
 
 
 
-resource "aws_security_group" "app_sg" {
+resource "aws_security_group" "application_sg" {
     name = "app_sg"
     vpc_id  = aws_vpc.vpc.id
     ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        security_group_id = "${aws_security_group.vpn_sg.id}"
+#        security_group_id = "${aws_security_group.vpn_sg.id}"
     }
     ingress {
         from_port = 5000
         to_port = 5000
         protocol = "tcp"
-        security_group_id = "${aws_security_group.vpn_sg.id}"
+#        security_group_id = "${aws_security_group.vpn_sg.id}"
     }
     tags {
         Name = "nginx_sg"
