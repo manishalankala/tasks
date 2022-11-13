5faaaf364eb1e194f50dd628c1dbe7172b138e2b
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 02:03:24 +0100

    Update nginx_securitygroup.tf

--- HL/nginx_securitygroup.tf
+++ HL/nginx_securitygroup.tf
@@ -7,13 +7,13 @@ resource "aws_security_group" "nginx_sg" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        security_group_id = ["${aws_security_group.vpn_sg.id}"]
+        security_group_id = "${aws_security_group.vpn_sg.id}"
     }
     ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        security_group_id = ["${aws_security_group.vpn_sg.id}"]
+        security_group_id = "${aws_security_group.vpn_sg.id}"
     }
     tags {
         Name = "nginx_sg"
