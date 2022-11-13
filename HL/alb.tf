41f669f499a52f187464b3ed4cce823f9d7dbda9
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 01:33:31 +0100

    Update alb.tf

--- HL/alb.tf
+++ HL/alb.tf
@@ -7,13 +7,15 @@ resource "aws_lb" "alb" {
   subnet_mapping {
     subnet_id     = aws_subnet.vpc_public_subnet_3.id
     allocation_id = aws_eip.alb_eip1.id
+#    private_ipv4_address = "xxxxxxx"
   }
 
   subnet_mapping {
     subnet_id     = aws_subnet.vpc_public_subnet_4.id
     allocation_id = aws_eip.alb_eip1.id
+#    private_ipv4_address = "xxxxxxxxx"
   }
 }
 
 
-#### Note eip for this alb is optional #####
+#### Note eip for this alb is optional or it can for internal with private ip #####
