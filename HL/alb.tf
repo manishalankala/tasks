9f37ef7e50560335e8e29bb8017590b12ca37307
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 01:21:53 +0100

    Create alb.tf

---
+++ HL/alb.tf
@@ -0,0 +1,13 @@
+resource "aws_lb" "alb" {
+  name               = "alb"
+  load_balancer_type = "application"
+  subnet_mapping {
+    subnet_id     = aws_subnet.vpc_public_subnet_3.id
+    allocation_id = aws_eip.example1.id
+  }
+
+  subnet_mapping {
+    subnet_id     = aws_subnet.vpc_public_subnet_4.id
+    allocation_id = aws_eip.example2.id
+  }
+}
