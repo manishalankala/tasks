017c13f036411bf240b86bd41da5aba8ecd69be9
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 01:17:09 +0100

    Update vpc.tf

--- HL/vpc.tf
+++ HL/vpc.tf
@@ -117,39 +117,6 @@ resource "aws_route_table_association" "vpc_private_sn_rt_assn" {
 
 
 
-####### security group Nginx #######
-
-resource "aws_security_group" "nginx_sg" {
-
-    vpc_id = "${aws_vpc.vpc_name.id}"
-    name   = "nginx_sg"
-  
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-        Name = "nginx_sg"
-    }
-}
 
 
 ####### security group Mongodb #######
