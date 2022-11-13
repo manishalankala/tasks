8e744e83ada2035fd3f22a7425f682c090ab4900
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 01:24:43 +0100

    Update alb.tf

--- HL/alb.tf
+++ HL/alb.tf
@@ -3,11 +3,11 @@ resource "aws_lb" "alb" {
   load_balancer_type = "application"
   subnet_mapping {
     subnet_id     = aws_subnet.vpc_public_subnet_3.id
-    allocation_id = aws_eip.example1.id
+    allocation_id = aws_eip.alb_eip1.id
   }
 
   subnet_mapping {
     subnet_id     = aws_subnet.vpc_public_subnet_4.id
-    allocation_id = aws_eip.example2.id
+    allocation_id = aws_eip.alb_eip1.id
   }
 }
