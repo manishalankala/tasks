56224007b0e7ec78bc7c5370a4c01011b396090d
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 01:39:09 +0100

    Update subnets.tf

--- HL/subnets.tf
+++ HL/subnets.tf
@@ -46,3 +46,82 @@ resource "aws_subnet" "vpc_public_subnet_4" {
     Name = "${var.vpc_name}_vpc_public_subnet_4"
   }
 }
+
+###### Public subnet additional ######
+
+resource "aws_subnet" "vpc_public_subnet_4" {
+  vpc_id            = "vpc"
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  map_public_ip_on_launch = "true"
+  tags {
+    Name = "${var.vpc_name}_vpc_public_subnet_4"
+  }
+}
+
+###### Public subnet additional ######
+
+resource "aws_subnet" "vpc_public_subnet_4" {
+  vpc_id            = "vpc"
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  map_public_ip_on_launch = "true"
+  tags {
+    Name = "${var.vpc_name}_vpc_public_subnet_4"
+  }
+}
+
+
+
+###### Private subnet ######
+
+resource "aws_subnet" "vpc_private_subnet_1" {
+  vpc_id            = "vpc"
+  cidr_block        = "10.0.7.0/24"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  map_public_ip_on_launch = "false"
+
+  tags {
+    Name = "${var.vpc_name}_vpc_private_sn_1"
+  }
+}
+
+###### Private subnet ######
+
+resource "aws_subnet" "vpc_private_subnet_2" {
+  vpc_id            = "vpc"
+  cidr_block        = "10.0.8.0/24"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  map_public_ip_on_launch = "false"
+
+  tags {
+    Name = "${var.vpc_name}_vpc_private_subnet_2"
+  }
+}
+
+
+###### Private subnet ######
+
+resource "aws_subnet" "vpc_private_subnet_3" {
+  vpc_id            = "vpc"
+  cidr_block        = "10.0.9.0/24"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  map_public_ip_on_launch = "false"
+
+  tags {
+    Name = "${var.vpc_name}_vpc_private_subnet_3"
+  }
+}
+
+###### Private subnet ######
+
+resource "aws_subnet" "vpc_private_subnet_4" {
+  vpc_id            = "vpc"
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  map_public_ip_on_launch = "false"
+
+  tags {
+    Name = "${var.vpc_name}_vpc_private_subnet_4"
+  }
+}
