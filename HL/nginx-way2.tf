8d78cd62d029871304ca433de614285f886d012d
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 01:10:21 +0100

    Update nginx-way2.tf

--- HL/nginx-way2.tf
+++ HL/nginx-way2.tf
@@ -14,12 +14,23 @@ resource "aws_autoscaling_group" "nginx_asg" {
   name                      = "nginx_asg"
   max_size                  = 2
   min_size                  = 2
+  desired_capacity          = 2
   health_check_grace_period = 300
   #availability_zones = var.availability_zones #["us-east-1a"]
   vpc_zone_identifier = aws_subnet.vpc_public_subnet_2.id
 
   launch_template {
     id      = aws_launch_template.template.id
+    version = "$Latest"
   }
+}
 
+
+resource "aws_autoscaling_policy" "nginx_asp" {
+  name                   = "nginx_asp"
+  autoscaling_group_name = aws_autoscaling_group.nginx_asg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "2" 
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
 }
