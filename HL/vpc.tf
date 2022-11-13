4b76d197f32eb027fa473bcd87c64c523797b558
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-13 01:34:43 +0100

    Update vpc.tf

--- HL/vpc.tf
+++ HL/vpc.tf
@@ -20,14 +20,6 @@ resource "aws_vpc" "vpc" {
 }
 
 
-###### Internet gateway ######
-
-resource "aws_internet_gateway" "vpc_igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  tags {
-    Name = "${var.vpc}_igw"
-  }
-}
 
 
 
