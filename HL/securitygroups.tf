15cb48c7091965d602442d35baf8d26a2cc38aa7
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-16 23:37:48 +0100

    Update securitygroups.tf

--- HL/securitygroups.tf
+++ HL/securitygroups.tf
@@ -50,48 +50,109 @@ resource "aws_security_group_rule" "vpn_egress_all" {
 }
 
 
-
-
-
-alb_external_sg
-
-alb_internal_sg
-
-  resource "aws_security_group" "vpn_sg" {
-  vpc_id = "vpc"
-  name   = "vpn_sg"
-
+  
+#-------------------------------------
+# Alb_External - Security_Group & Rules
+#-------------------------------------  
 
 resource "aws_security_group" "alb_external_sg" {
-    name = "nginx_sg"
-    vpc_id  = aws_vpc.vpc.id
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        security_group_id = "${aws_security_group.vpn_sg.id}"
-    }
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        security_group_id = "${aws_security_group.vpn_sg.id}"
-    }
+  vpc_id = "vpc"
+  name   = "alb_external_sg"
+
+resource "aws_security_group_rule" "alb_external_ingress_80" {
+  security_group_id = "${aws_security_group.alb_external_sg.id}"
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["${var.vpc_public_subnet_2}","${var.vpc_public_subnet_3}"]
+}
+
+resource "aws_security_group_rule" "alb_external_ingress_8080" {
+  security_group_id = "${aws_security_group.alb_external_sg.id}"
+  type = "ingress"
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["${var.vpc_public_subnet_3}","${var.vpc_public_subnet_4}"]
+}
+
+resource "aws_security_group_rule" "alb_external_ingress_443" {
+  security_group_id = "${aws_security_group.alb_external_sg.id}"
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["${var.vpc_public_subnet_3}","${var.vpc_public_subnet_4}"]
+}
+
+resource "aws_security_group_rule" "alb_external_egress_all" {
+  security_group_id = "${aws_security_group.alb_external_sg.id}"
+  type = "egress"
+  from_port = 0
+  to_port = 65535
+  protocol = "-1"
+  cidr_blocks = ["${var.vpc_public_subnet_3}","${var.vpc_public_subnet_4}"]
+}
+  
+ 
+
+#-------------------------------------
+# Alb_Internal - Security_Group & Rules
+#-------------------------------------  
+
+resource "aws_security_group" "alb_internal_sg" {
+  vpc_id = "vpc"
+  name   = "alb_internal_sg"
+
+resource "aws_security_group_rule" "alb_internal_ingress_80" {
+  security_group_id = "${aws_security_group.alb_internal.id}"
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["${var.vpc_public_subnet_3}","${var.vpc_public_subnet_4}"]
+}
+
+resource "aws_security_group_rule" "alb_internal_ingress_3000" {
+  security_group_id = "${aws_security_group.alb_external_sg.id}"
+  type = "ingress"
+  from_port = 3000
+  to_port = 3000
+  protocol = "tcp"
+  cidr_blocks = ["${var.vpc_public_subnet_3}","${var.vpc_public_subnet_4}"]
+}
+
+resource "aws_security_group_rule" "alb_internal_ingress_443" {
+  security_group_id = "${aws_security_group.alb_external_sg.id}"
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["${var.vpc_public_subnet_3}","${var.vpc_public_subnet_4}"]
+}
+
+resource "aws_security_group_rule" "alb_internal_egress_all" {
+  security_group_id = "${aws_security_group.alb_internal_sg.id}"
+  type = "egress"
+  from_port = 0
+  to_port = 65535
+  protocol = "-1"
+  cidr_blocks = ["${var.vpc_public_subnet_3}","${var.vpc_public_subnet_4}"]
 }
 
 
-  
-  
-  
-  
+#-------------------------------------
+# Mongodb - Security_Group & Rules
+#-------------------------------------  
+     
 resource "aws_security_group" "mongodb_sg" {
   vpc_id      = "${aws_vpc.vpc.id}"
-
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${ }"]
+    cidr_blocks = ["${var.vpc_private_subnet_2}","${var.vpc_private_subnet_3}"]
   }
   ingress {
     from_port   = 27017
