4bf1b0bd4b2d7833d0fde9115f6021503cbff288
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-16 17:46:52 +0100

    Update internal_loadbalancer.tf

--- HL/internal_loadbalancer.tf
+++ HL/internal_loadbalancer.tf
@@ -1,17 +1,1 @@
-resource "aws_lb" "internal_lb" {
-  name               = "internal_lb"
-  internal           = true
-  enable_deletion_protection = true 
-  subnet_mapping {
-    subnet_id            = aws_subnet.xx.id
-    private_ipv4_address = "xxxxxxx"
-  }
-  subnet_mapping {
-    subnet_id            = aws_subnet.xxx.id
-    private_ipv4_address = "xxxxxxx"
-  }  
-  subnet_mapping {
-    subnet_id            = aws_subnet.xxxx.id
-    private_ipv4_address = "xxxxxxx"
-  }  
-}
+
