ed55e9645e17d9b5d99f99a57df41d9b6f455e2b
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-16 22:47:30 +0100

    Update securitygroups.tf

--- HL/securitygroups.tf
+++ HL/securitygroups.tf
@@ -4,58 +4,79 @@
 # resource "aws_security_group_rule"
 
 
+#-------------------------------------
+# VPN - Security_Group & Rules
+#-------------------------------------
+
+resource "aws_security_group" "vpn_sg" {
+  vpc_id = "vpc"
+  name   = "vpn_sg"
+
+resource "aws_security_group_rule" "vpn_ingress_443" {
+  security_group_id = "${aws_security_group.vpn_sg.id}"
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["${var.vpc_public_subnet_1"]
+}
+
+resource "aws_security_group_rule" "vpn_ingress_943" {
+  security_group_id = "${aws_security_group.vpn_sg.id}"
+  type = "ingress"
+  from_port = 943
+  to_port = 943
+  protocol = "tcp"
+  cidr_blocks = ["${var.vpc_public_subnet_1"]
+}
+
+resource "aws_security_group_rule" "vpn_ingress_1194" {
+  security_group_id = "${aws_security_group.vpn_sg.id}"
+  type = "ingress"
+  from_port = 1194
+  to_port = 1194
+  protocol = "udp"
+  cidr_blocks = ["${var.vpc_public_subnet_1"]
+}
+
+
+resource "aws_security_group_rule" "vpn_egress_all" {
+  security_group_id = "${aws_security_group.vpn_sg.id}"
+  type = "egress"
+  from_port = 0
+  to_port = 65535
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+
+
+
+
 alb_external_sg
 
 alb_internal_sg
 
 
-
-resource "aws_security_group" "vpn_sg" {
-
-    vpc_id = "vpc"
-    name   = "vpn_sg"
-  
+resource "aws_security_group" "nginx_sg" {
+    name = "nginx_sg"
+    vpc_id  = aws_vpc.vpc.id
     ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        
-        cidr_blocks = ["0.0.0.0/0"]
+        security_group_id = "${aws_security_group.vpn_sg.id}"
     }
-
     ingress {
-        from_port = 943
-        to_port = 943
+        from_port = 80
+        to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        security_group_id = "${aws_security_group.vpn_sg.id}"
     }
-  
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-    ingress {
-        from_port = 1194
-        to_port = 1194
-        protocol = "udp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-        Name = "vpn_sg"
+    tags {
+        Name = "nginx_sg"
     }
 }
-
-
 
 
 resource "aws_security_group" "mongodb_sg" {
