2bb5036f0d86a0b359b6b84909593233aae60e88
bWFuaXNoIGt1bWFyIGFsYW5rYWxh <33985509+manishalankala@users.noreply.github.com>
2022-11-16 18:53:49 +0100

    Update alb.tf

--- HL/alb.tf
+++ HL/alb.tf
@@ -6,7 +6,9 @@
 ### listener for the load balancer
 
 
-
+#-------------------------------------
+# External load balancer
+#-------------------------------------
 
 resource "aws_lb" "alb_external" {
   name                 = "alb_external"
@@ -17,11 +19,111 @@ resource "aws_lb" "alb_external" {
   security_groups = [ aws_security_group.alb_external_sg.id]                          ]
 }
 
+#-------------------------------------
+# External load balancer - Target Group 1
+#-------------------------------------
+
+resource "aws_lb_target_group" "nginx1-tg" {
+  name     = "nginx2_tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  deregistration_delay = 60
+  stickiness {
+    enabled = false
+    type    = "lb_cookie"
+    cookie_duration = 60
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = 200
+
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#-------------------------------------
+# External load balancer - Target Group 2
+#-------------------------------------
+
+resource "aws_lb_target_group" "nginx2_tg" {
+  name     = "nginx2_tg"
+  port     = 443
+  protocol = "HTTPS"
+  vpc_id   = aws_vpc.main.id
+  deregistration_delay = 60
+  stickiness {
+    enabled = false
+    type    = "lb_cookie"
+    cookie_duration = 60
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTPS"
+    matcher             = 200
+
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 
 
 
+#========================================================
+#  External load balancer - http listener 
+#========================================================
 
+resource "aws_lb_listener" "listner" {
+  
+  load_balancer_arn = aws_lb.lb.id
+  port              = 80
+  protocol          = "HTTP"
+  
+#-------------------------------------
+#defualt action of the target group.
+#-------------------------------------
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = " No such Site Found"
+      status_code  = "200"
+   }
+}
+    
+  depends_on = [  aws_lb.alb_external ]
+}
+
+
+
+resource "aws_lb_listener" "nginx_ls" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
+  }
+}
 
 
 
